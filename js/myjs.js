7f523f2eb4588e554e37998a849a7c082ca00c65
Dong Liang <dongliang3571@gmail.com>
2016-01-18 16:42:53 -0500

    added project items

---
+++ js/myjs.js
@@ -0,0 +1,25 @@
+$(function(){
+    $smallimage = $("a.smallimage");
+
+    $smallimage.on('click', function(event){
+      var imgurl = $(this).attr('href');
+      event.preventDefault();
+      var display_image = '<div class="blackback"></div><div class="popimage"><a class="close"><span class="glyphicon glyphicon-remove"></span></a></div>';
+
+      $('body').prepend(display_image);
+      $('div.popimage').css('background-image', 'url(' + imgurl + ')');
+      $a_close = $("a.close");
+      $a_close.on('click', function(){
+        $('div.popimage').remove();
+        $('div.blackback').remove();
+      });
+
+      $('div.blackback').on('click', function(){
+        $('div.popimage').remove();
+        $('div.blackback').remove();
+      });
+
+    });
+
+
+});
